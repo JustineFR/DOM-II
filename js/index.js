c6532847e12329f360fc733e9aa6349b719736ea
Justine Gennaro <justine.rouzier@gmail.com>
2019-08-06 12:57:54 -0500

    The nav links turn pink when clicked

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+// The nav bar turns blue on mouseover
+const navBar = document.querySelector('.nav-container')
+navBar.addEventListener('mouseover', () => {
+    navBar.style.backgroundColor = "#33E9FD";
+})
+
+// The nav bar goes back to original color on mouseout
+navBar.addEventListener('mouseout', () => {
+    navBar.style.backgroundColor = "#ffffff";
+})
+
+// The nav links turns 
+const navLinks = document.querySelectorAll('.nav-link')
+navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+    link.style.color = '#D533FD';
+    })
+})
